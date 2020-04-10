1b77728f468725aa6fadaf213f8b793412c897eb
Sunil Karki <karkisunil1200@gmail.com>
2020-04-10 10:48:56 -0500

    Fetching all the projects with the ID provided

--- api/project-router.js
+++ api/project-router.js
@@ -6,10 +6,26 @@ const router = express.Router();
 router.get('/', (req, res) => {
   db.get()
     .then(projects => {
-      res.status(201).json(projects);
+      res.status(200).json(projects);
     })
     .catch(err => {
-      res.status(500).json({message: 'Could not retrieve any projects'});
+      res.status(500).json({message: 'Could not retrieve any projects', err: err.message});
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const {id} = req.params;
+
+  db.get(id)
+    .then(project => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({message: 'The project with that ID does not exist'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Could not fetch project for that ID'});
     });
 });
 
